861e3bff3d49569ea54fd4698ee6bc5c4ad8540d
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2020-05-26 20:15:55 -0300

    adicionando recursos base para aplicação

---
+++ base/Feriado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calendario
+{
+    public class Feriado
+    {
+        public int Dia { get; set; }
+        public int Mes { get; set; }
+        public string Descricao { get; set; }
+
+        public Feriado(int dia, int mes, string descricao)
+        {
+            Dia = dia;
+            Mes = mes;
+            Descricao = descricao;
+        }
+        public static Feriado ObterFeriadoComIntervaloFixo(DateTime DomingoDePascoa, int intervalo, string descricao)
+        {
+            return new Feriado(DomingoDePascoa.AddDays(intervalo).Day, DomingoDePascoa.AddDays(intervalo).Month, descricao);
+        }
+    }
+}
